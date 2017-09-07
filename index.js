071a8a6ab735101c37374d84726610d716d6d2a8
Haiyang Lou <haiyanglou@HaiyangdeMacBook-Air.local>
2017-09-07 01:04:29 -0500

    New Version

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ $(document).ready(function() {
 
 $(function() {
 
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top - 60
-        }, 400);
-        return false;
-      }
-    }
-  });
+	$('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: target.offset().top - 60
+				}, 400);
+				return false;
+			}
+		}
+	});
 
-  $('#carousel').carousel({
-    interval: 2500
-  })  
+	$('#carousel').carousel({
+		interval: 2500
+	})  
 
 });
