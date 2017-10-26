343bb98a799effb43ccdce810af6ba9ced11aaba
Haiyang Lou <haiyanglou@HaiyangdeMacBook-Air.local>
2017-10-25 22:44:14 -0500

    Update info

--- README.md
+++
@@ -1,3 +0,0 @@
-# My Personal Website
-
-www.utdallas.edu/~hxl164530
--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+	$('#vertical').lightSlider({
+      gallery:true,
+      item:1,
+      vertical:true,
+      verticalHeight:295,
+      vThumbWidth:50,
+      thumbItem:8,
+      thumbMargin:4,
+      slideMargin:0
+    });  
 });
 
 $(function() {
@@ -21,3 +31,4 @@ $(function() {
 	});  
 
 });
+
