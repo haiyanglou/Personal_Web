f126c90261dbb952acb949f875e1731e18df7b76
haiyanglou <haiyanglou@HaiyangdeMacBook-Air.local>
2017-11-03 03:29:23 -0500

    css,html,js file update

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 $(document).ready(function() {
-	$('#vertical').lightSlider({
-      gallery:true,
-      item:1,
-      vertical:true,
-      verticalHeight:295,
-      vThumbWidth:50,
-      thumbItem:8,
-      thumbMargin:4,
-      slideMargin:0
-    });  
 });
 
 $(function() {
